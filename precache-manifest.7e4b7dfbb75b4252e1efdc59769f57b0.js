cfe25e1b4c096e91cda8c8d5af844441a60b0f90
MarcMarabou <17163097+MarcMarabou@users.noreply.github.com>
2020-05-28 21:41:04 +0200

    Initial commit

---
+++ precache-manifest.7e4b7dfbb75b4252e1efdc59769f57b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be423decefb7714c13a54cd87678427",
+    "url": "/chora/index.html"
+  },
+  {
+    "revision": "2293832bbfe4a208b94c",
+    "url": "/chora/static/css/2.56a1fa8e.chunk.css"
+  },
+  {
+    "revision": "e5943a4694a84692f05f",
+    "url": "/chora/static/css/main.c74809f5.chunk.css"
+  },
+  {
+    "revision": "2293832bbfe4a208b94c",
+    "url": "/chora/static/js/2.24da4f73.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/chora/static/js/2.24da4f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5943a4694a84692f05f",
+    "url": "/chora/static/js/main.4cda78b7.chunk.js"
+  },
+  {
+    "revision": "55a124a44a64bd69e867",
+    "url": "/chora/static/js/runtime-main.2c7a1063.js"
+  }
+]);
